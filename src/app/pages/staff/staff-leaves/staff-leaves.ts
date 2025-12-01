30c701add98a64371c6958473a78ed5183eb7d4d
Tejas Kedare <tejaskedare2@gmail.com>
2025-12-01 10:18:18 +0530

    added validations to from and to date

--- src/app/pages/staff/staff-leaves/staff-leaves.ts
+++ src/app/pages/staff/staff-leaves/staff-leaves.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HodDataService } from '../../../services/hod.service';
 import { AuthService } from '../../../services/auth.service';
-import { FormBuilder, ReactiveFormsModule, Validators, FormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators, FormsModule, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-staff-leaves',
@@ -27,6 +27,25 @@ export class StaffLeaves implements OnInit {
       reason: ['', Validators.required]
     });
 
+    this.form.get('fromDate')!.valueChanges.subscribe((from: string) => {
+      const toCtrl = this.form.get('toDate')!;
+
+      toCtrl.reset();
+      toCtrl.disable();
+
+      if (from) {
+        toCtrl.enable();
+        toCtrl.setValue('');
+        toCtrl.setValidators([
+          Validators.required,
+          (control: AbstractControl) => {
+            if (!control.value) return null;
+            return control.value < from ? { invalidRange: true } : null;
+          }
+        ]);
+        toCtrl.updateValueAndValidity();
+      }
+    });
 
     const user = this.auth.getCurrentUserSync();
     if (!user) return;
@@ -38,19 +57,19 @@ export class StaffLeaves implements OnInit {
     this.leaves = this.hodData.listLeavesByStaff(this.staffId);
   }
 
-apply() {
-  if (this.form.invalid) return;
+  apply() {
+    if (this.form.invalid) return;
 
-  this.hodData.applyLeave({
-    fromDate: this.form.value.fromDate,
-    toDate: this.form.value.toDate,
-    reason: this.form.value.reason
-  });
+    this.hodData.applyLeave({
+      fromDate: this.form.value.fromDate,
+      toDate: this.form.value.toDate,
+      reason: this.form.value.reason
+    });
 
-  this.form.reset();
-  this.showApply = false;
-  this.load();
-}
+    this.form.reset();
+    this.showApply = false;
+    this.load();
+  }
 
 
   view(l: any) {
